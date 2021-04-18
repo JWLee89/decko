bf34ef2e43f6f460f4ab0f67f52b49d584a2b028
Jay Lee <ljay189@gmail.com>
2021-04-18 18:21:56 +0900

    Added simple test and an empty requirements.txt

---
+++ tests/test_experiment.py
@@ -0,0 +1,15 @@
+from src.experiment.util import TimeComputer
+
+
+def test_time_computer_outputs_float():
+    """
+    Check to see if the TimeComputer() outputs a float value.
+    :return: None
+    """
+    time_taken = []
+    for i in range(100):
+        with TimeComputer(time_taken):
+            dummy_list = list(range(10000))
+    mean_time_ms = TimeComputer.compute_avg_time(time_taken, TimeComputer.Units.MS)
+    assert isinstance(mean_time_ms, float)
+
