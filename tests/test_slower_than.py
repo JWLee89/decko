2d8b67ea8d6b40af0976586f5d83210b6dbbeb4d
JWLee89 <ljay189@gmail.com>
2021-05-15 13:30:02 +0900

    Updated unit test to thow error

--- tests/test_slower_than.py
+++ tests/test_slower_than.py
@@ -6,16 +6,16 @@ dk = Decko(__name__, debug=True)
 
 @pytest.mark.parametrize("input_size",
     [
-        100000,
-        1000000,
         10000000,
+        20000000,
+        30000000,
     ]
 )
 @pytest.mark.parametrize("milliseconds",
     [
-        1000,
-        2000,
-        3000,
+        100,
+        200,
+        300,
     ]
 )
 def test_slower_than(input_size, milliseconds):
@@ -30,4 +30,6 @@ def test_slower_than(input_size, milliseconds):
             x += i
         return x
 
-    long_func(input_size)
+    # Ideally these functions should take longer than 300 milliseconds to execute
+    with pytest.raises(ValueError) as err:
+        long_func(input_size)
