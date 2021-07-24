699c5fe7ff0ba0581266154761718b0e9d388e5f
jaylee <ljay189@gmail.com>
2021-07-24 15:26:53 +0900

    Updated unit test

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -4,19 +4,14 @@ from src.decko.debug import (
 )
 
 
-@pytest.mark.parametrize("threshold_value", [
-    5,
-    600
+@pytest.mark.parametrize("test_case", [
+    (2, 5, 5),
+    (200, 500, 600)
 ])
-@pytest.mark.parametrize("a_and_b", [
-    (2, 5),
-    (200, 500)
-])
-def test_raise_errors_if(threshold_value, a_and_b):
-    first_num, second_num = a_and_b
+def test_raise_errors_if(test_case):
+    first_num, second_num, threshold_value = test_case
 
     def sum_greater_than_threshold(total_sum):
-        print(f"threshold: {threshold_value}, sum: {total_sum}")
         return threshold_value < total_sum
 
     @raise_error_if(sum_greater_than_threshold)
